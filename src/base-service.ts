f89ac3db9d96a095b9cfb8b25c07f260fa636092
Roy Art <royart@me.com>
2018-06-01 09:48:55 +0200

    Responding with same content as current lib responds (returning the `data` property of the response).

--- src/base-service.ts
+++ src/base-service.ts
@@ -1,6 +1,7 @@
-import { ajax } from 'rxjs/ajax';
+import { ajax, AjaxResponse } from 'rxjs/ajax';
 import { Observable } from 'rxjs';
 import { StudioConfig } from './studio-config';
+import { map } from 'rxjs/operators';
 
 export class BaseService {
   protected constructor(public config: StudioConfig) {
@@ -9,11 +10,13 @@ export class BaseService {
 
   static httpGet(requestURL: string, params: Object = {}): Observable {
     const searchParams = new URLSearchParams(params as URLSearchParams);
-    return ajax.get(`${requestURL}${searchParams.toString()}`);
+    return ajax.get(`${requestURL}${searchParams.toString()}`)
+      .pipe(map((ajaxResponse: AjaxResponse) => ajaxResponse.response.data));
   }
 
   static httpPost(requestURL: string, body: Object = {}): Observable {
-    return ajax.post(requestURL, body);
+    return ajax.post(requestURL, body)
+      .pipe(map((ajaxResponse: AjaxResponse) => ajaxResponse.response.data));
   }
 
 }
