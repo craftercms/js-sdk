0615cd92cd1994357d2e7e4162d76571286aeb4e
jvega190 <josevega19@gmail.com>
2021-10-08 16:12:39 -0600

    Update PartialObserver import - 5025

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -14,7 +14,7 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
 
-import { PartialObserver } from 'rxjs/index';
+import { PartialObserver } from 'rxjs';
 
 export declare type ObserverOrNext<T> = (value: T) => void | PartialObserver<T>;
 
