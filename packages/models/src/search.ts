38e7f94d214c70b7cc987dc1607cdcf4aac92d06
jvega190 <josevega19@gmail.com>
2021-10-12 11:29:03 -0600

    Add search model - 5025

---
+++ packages/models/src/search.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2007-2021 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
+import { LookupTable } from "@craftercms/models";
+
+export interface resultHit {
+  _id: string;
+  _index: string;
+  _score: string;
+  _source: LookupTable<any>;
+  _type: string;
+}
+
+export interface SearchResult {
+  hits: resultHit[];
+  max_score: number;
+  total: {
+    relation: string;
+    value: number;
+  }
+}
